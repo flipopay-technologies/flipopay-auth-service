c99d0ea27616eb9dc7dc351b9683d92c20afdc81
raam-techie <rajaramjayaraman.j@gmail.com>
2024-12-31 00:46:19 +0530

    feat: :arrow_up: Added grpc dependencies and folder structure

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,10 +2,25 @@ package app
 
 import (
 	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 func StartServer() {
 
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 	log.Println("Application started")
+
+	grpcServer := grpc.NewServer()
+
+	log.Println("Grpc server started")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 
 }
