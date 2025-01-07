f0def977f934fd9bf18d1dee6034c8b283388e0d
raam-techie <rajaramjayaraman.j@gmail.com>
2025-01-08 02:30:09 +0530

    refactor: :hammer: cicd test

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,25 +2,22 @@ package app
 
 import (
 	"log"
-	"net"
-
-	"google.golang.org/grpc"
 )
 
 func StartServer() {
 
-	lis, err := net.Listen("tcp", ":9000")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+	// lis, err := net.Listen("tcp", ":9000")
+	// if err != nil {
+	// 	log.Fatalf("failed to listen: %v", err)
+	// }
 	log.Println("Application started")
 
-	grpcServer := grpc.NewServer()
+	// grpcServer := grpc.NewServer()
 
-	log.Println("Grpc server started")
+	// log.Println("Grpc server started")
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
-	}
+	// if err := grpcServer.Serve(lis); err != nil {
+	// 	log.Fatalf("failed to serve: %s", err)
+	// }
 
 }
